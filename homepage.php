42b4d511e959203c288c7d90324a357645818b28
GUILHERME BENJAMIN DA SILVA AGUIAR - SENAI <cpds1214411@hotmail.com>
2024-08-13 10:10:42 -0300

    first commit

---
+++ homepage.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Mirror Fashion</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" href="css/reset.css"> 
+        <link rel="stylesheet" href="css/estilos.css">
+        <link rel="stylesheet" href="css/mobile.css" media="(max-width:	939px)">
+    </head>
+<body>
+    <header class="container">
+        <h1><img src="img/logo.png" alt="Mirror Fashion"></h1>
+    <p class="sacola">
+        Nenhum item na sacola de compras
+    </p>
+    
+    <nav class="menu-opcoes">
+        <ul>
+            <li><a href="#">Sua Conta</a></li>
+            <li><a href="#">Lista de Desejos</a></li>
+            <li><a href="#">Cartão Fidelidade</a></li>
+            <li><a href="sobre.html">Sobre</a></li>
+            <li><a href="#">Ajuda</a></li>
+        </ul>    
+        </nav>
+    </header>
+    <div class="container destaque">
+        <section class="busca">
+        <h2>Busca</h2>
+
+        <form>
+            <input type="search">
+            <input type="image" src="img/busca.png">
+        </form>
+    </section>
+
+    <section class="menu-departamentos">
+        <h2>Departamentos</h2>
+
+        <nav>
+            <ul>
+                <li><a	href="#">Blusas	e	Camisas</a>
+                    <ul>
+                        <li><a	href="#">Manga	curta</a></li>
+                        <li><a	href="#">Manga	comprida</a></li>
+                        <li><a	href="#">Camisa	social</a></li>
+                        <li><a	href="#">Camisa	casual</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Calças</a>
+                <li><a href="#">Saias</a>
+                <li><a href="#">Vestidos</a>
+                <li><a href="#">Sapatos</a>
+                <li><a href="#">Bolsas e Carteiras</a>
+                <li><a href="#">Acessórios</a>
+            </ul>
+    
+        </nav>
+
+        
+
+
+
+    </section>
+
+    <img src="img/destaque-home.png" alt="Promoção: Big City Night">
+    </div>
+
+    <div class="container paineis">
+        <section class="painel novidades">
+			<h2>Novidades</h2>
+			<ol>
+				<li>
+				    <a href="produto.html">
+						<figure>
+						    <img src="img/produtos/miniatura1.png" style="width: 130px;">
+								<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+                            </figure>
+                    </a>
+                </li>
+                
+                <li>
+                    <a href="">
+                        <figure>
+                            <img src="img/produtos/foto3-azul.png" style="width: 130px;">
+                                <figcaption>Teste</figcaption>
+                        </figure>
+                    </a>
+                </li>
+                
+                <li>
+                <a href="">
+                        <figure>
+                            <img src="img/produtos/foto4-rosa.png" style="width: 130px;">
+                                <figcaption>Moletom</figcaption>
+                        </figure>
+                    </a>
+                </li>
+                
+                <li>
+                    <a href="">
+                        <figure>
+                            <img src="img/produtos/foto5-rosa.png" style="width: 130px;">
+                                <figcaption>Camisa regata</figcaption>
+                        </figure>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="">
+                        <figure>
+                            <img src="img/produtos/foto15-azul.png" style="width: 130px;">
+                                <figcaption>Vestido Azul </figcaption>
+                        </figure>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="">
+                        <figure>
+                            <img src="img/produtos/foto8-verde.png" style="width: 130px;">
+                                <figcaption>Camisa Verde</figcaption>
+                        </figure>
+                    </a>
+				</li>
+
+			</ol>
+        </section>
+                
+        <section class="painel mais-vendidos">
+                    <h2>Mais Vendidos</h2>
+                        <ol>
+                            <li>
+                                <a href="produto.html">
+                                    <figure>
+                                        <img src="img/produtos/foto1-verde.png" style="width: 130px;">
+                                            <figcaption>Camisa Manga Longa</figcaption>
+                                        </figure>
+                                </a>
+                            </li>
+                            
+                            <li>
+                                <a href="">
+                                    <figure>
+                                        <img src="img/produtos/foto2-rosa.png" style="width: 130px;">
+                                            <figcaption>Camisa Rosa</figcaption>
+                                    </figure>
+                                </a>
+                            </li>
+                            
+                            <li>
+                            <a href="">
+                                    <figure>
+                                        <img src="img/produtos/foto3-azul.png" style="width: 130px;">
+                                            <figcaption>Camisa Manga Longa Azul</figcaption>
+                                    </figure>
+                                </a>
+                            </li>
+                            
+                            <li>
+                                <a href="">
+                                    <figure>
+                                        <img src="img/produtos/foto4-verde.png" style="width: 130px;">
+                                            <figcaption>Jaqueta Verde</figcaption>
+                                    </figure>
+                                </a>
+                            </li>
+            
+                            <li>
+                                <a href="">
+                                    <figure>
+                                        <img src="img/produtos/foto9-rosa.png" style="width: 130px;">
+                                            <figcaption>Blusa Xadrex</figcaption>
+                                    </figure>
+                                </a>
+                            </li>
+            
+                            <li>
+                                <a href="">
+                                    <figure>
+                                        <img src="img/produtos/miniatura10.png" style="width: 130px;">
+                                            <figcaption>Blusa Rosa</figcaption>
+                                    </figure>
+                                </a>
+                            </li>
+                        </ol>
+        </section>
+    </div>
+
+        <footer>
+			<div	class="container">
+					<img src="img/logo-rodape.png" alt="Logo	Mirror	Fashion">
+					<ul	class="social">
+							<li><a href="http://facebook.com/mirrorfashion">Facebook</a></li>
+							<li><a href="http://twitter.com/mirrorfashion">Twitter</a></li>
+							<li><a href="http://plus.google.com/mirrorfashion">Google+</a></li>
+					</ul>
+			</div>
+        </footer>
+
+    </body>
+</html>
