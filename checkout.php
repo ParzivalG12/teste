7b72683913c26f143675c0bd0a9c6d07e774ed7b
GUILHERME BENJAMIN DA SILVA AGUIAR - SENAI <cpds1214411@hotmail.com>
2024-08-19 10:02:56 -0300

    first commit

--- checkout.php
+++ checkout.php
@@ -12,30 +12,38 @@
 	<div class="jumbotron">
 		<div class="container">
 			<h1>Ótima escolha!</h1>
-			<p>Obrigado por comprar na Mirror Fashion!
-				Preencha seus dados para efetivar a compra.</p>
+			<p>Obrigado por comprar na Mirror Fashion! Preencha seus dados para efetivar a compra.</p>
 		</div>
 	</div>
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-4">
-				<div class="panel	panel-default">
+				<div class="panel panel-default">
 					<div class="panel-heading">
 						<h2 class="panel-title">Sua compra</h2>
-					</div><!--	fim	.panel-heading	-->
+					</div><!-- fim .panel-heading -->
 					<div class="panel-body">
-						<img src="img/produtos/foto1-<?= $_POST['cor']	?>.png" class="img-thumbnail	img-responsive">
+						<img src="img/produtos/foto1-<?= $_POST["cor"] ?>.jpg" class="img-thumbnail img-responsive">
 						<dl>
-							<dt>Cor</dt>
-							<dd><?= $_POST['cor'] ?></dd>
-							<dt>Tamanho</dt>
-							<dd><?= $_POST['tamanho'] ?></dd>
 							<dt>Produto</dt>
-							<dd><?= $_POST['nome'] ?></dd>
+							<dd><?= $_POST["nome"] ?></dd>
 							<dt>Preço</dt>
-							<dd>R$ <?= $_POST['preco']	?></dd>
-
+							<dd id="preco">R$ <?= $_POST["preco"] ?></dd>
+							<dt>Cor</dt>
+							<dd><?= $_POST["cor"] ?></dd>
+							<dt>Tamanho</dt>
+							<dd><?= $_POST["tamanho"] ?></dd>
 						</dl>
+						<div class="form-group">
+							<label for="qt">Quantidade</label>
+							<input id="qt" class="form-control" type="number" min="0" max="99" value="1">
+						</div>
+						<div class="form-group">
+							<label for="total">Total</label>
+							<output for="qt preco" id="total" class="form-control">
+								<?= $_POST["preco"] ?>
+							</output>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -57,7 +65,7 @@
 
 						<div class="form-group">
 							<label for="cpf">CPF</label>
-							<input type="text" class="form-control" id="cpf" name="cpf" placeholder="000.000.000-00" required>
+							<input type="text" class="form-control" id="cpf" name="cpf" placeholder="000.000.000-00" data-mask="999.999.999-99" required>
 						</div>
 						<div class="checkbox">
 							<label>
@@ -70,7 +78,7 @@
 						<legend>Cartão de crédito</legend>
 						<div class="form-group">
 							<label for="numero-cartao">Número - CVV</label>
-							<input type="text" class="form-control" id="numero-cartao" name="numero-cartao">
+							<input type="text" class="form-control" id="numero-cartao" name="numero-cartao" data-mask="9999 9999 9999 9999 - 999">
 						</div>
 						<div class="form-group">
 							<label for="bandeira-cartao">Bandeira</label>
@@ -85,15 +93,20 @@
 							<input type="month" class="form-control" id="validade-cartao" name="validade-cartao">
 						</div>
 					</fieldset>
-					<button type="submit" class="btn btn-primary" class="btn-lg" class="pull-right">
-						<span class="glyphicon	glyphicon-thumbs-up"></span>
+					<button type="submit" class="btn btn-primary btn-lg pull-right">
+						<span class="glyphicon glyphicon-thumbs-up"></span>
 						Confirmar Pedido
 					</button>
-
+				</div>
 			</form>
 		</div>
 	</div>
 
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.js"></script>
+	<script src="js/inputmaskplugin.js"></script>
+	<script src="js/total.js"></script>
+	
 
 </body>
 
